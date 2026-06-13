dd2b5b5e64657b1cfb839131b6bd78920ff9ad9f
gamingzonepr23-hash <gamingzonepr23@gmail.com>
2026-06-13 18:06:20 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.JR_FIREBASE_CONFIG = {
-  enabled: false,
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
-};
